820a35444a1622f32c9ae0110e275f9fcc8a4bfb
Andres Soibelzon <a.soibelzon@gmail.com>
2022-12-14 18:58:05 +0000

    solving the problem

--- src/app.js
+++ src/app.js
@@ -6,6 +6,16 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let com = ["com", "us"];
+
+  let pronounRandom = pronoun[Math.floor(Math.random() * 1) + 0];
+  console.log();
+
+  return (document.getElementById("text").innerHTML = pronounRandom);
   console.log("Hello Rigo from the console!");
 };
+
+// Math.random() * (max - min) + min;
